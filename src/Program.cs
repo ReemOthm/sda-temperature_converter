6b5d04be747590933b685b7e88506936f0e0d62a
ReemOthm <ReemOth@outlook.com>
2024-03-18 18:18:14 +0300

    created temp convert function

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static string TempConvert(double tempValue, string tempUnit)
+    {
+        double result;
+        switch (tempUnit)
+        {
+            case "C":
+                result = (tempValue * 1.8) + 32;
+                return $"{result} F";
+            case "F":
+                result = (tempValue - 32) / 1.8;
+                return $"{result} C";
+            default:
+                return "Done";
+        }
+    }
+    public static void Main()
+    {
+        Console.WriteLine(TempConvert(32, "F"));
+        Console.WriteLine(TempConvert(100, "C"));
+    }
+}
