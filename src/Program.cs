4ccd2a1b9e0e772fe214e5dc773d41ac64cf604b
ReemOthm <ReemOth@outlook.com>
2024-03-18 18:44:47 +0300

    added user prompt

--- src/Program.cs
+++ src/Program.cs
@@ -7,17 +7,32 @@
         {
             case "C":
                 result = (tempValue * 1.8) + 32;
-                return $"{result} F";
+                return $"{result:F2} F";
             case "F":
                 result = (tempValue - 32) / 1.8;
-                return $"{result} C";
+                return $"{result:F2} C";
             default:
                 return "Done";
         }
     }
     public static void Main()
     {
+
         Console.WriteLine(TempConvert(32, "F"));
         Console.WriteLine(TempConvert(100, "C"));
+
+        Console.WriteLine("--------------Tempretures Converted-------------");
+
+        Console.Write("Enter a temperature and its unit (C or F): ");
+        string temperature = Console.ReadLine() ?? "";
+
+        string[] temperatureArray = temperature.Split(" ");
+        double tempValue = Convert.ToDouble(temperatureArray[0]);
+        string tempUnit = temperatureArray[1].ToUpper();
+
+        string convertedTempreture = TempConvert(tempValue, tempUnit);
+
+        Console.WriteLine($"Converted: {tempValue} {tempUnit} = {convertedTempreture}");
+
     }
 }
